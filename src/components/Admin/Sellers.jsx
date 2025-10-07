8b0338f99ab80b6aa0e733c9fab293cd7cc9be97
danieldgtal <danielchiatuiro@gmail.com>
2025-10-07 18:23:26 -0400

    api handling exercises

--- src/components/Admin/Sellers.jsx
+++ src/components/Admin/Sellers.jsx
@@ -1,22 +1,85 @@
 import React, { useEffect, useState } from "react";
+import apiClient from "../../utils/api-client";
+import Loader from "../Common/Loader";
 
 const Sellers = () => {
 	const [name, setName] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+	const [errors, setErrors] = useState(null);
+	const [sellers, setSellers] = useState([]);
 
+	// using apiClient to api calls
 	useEffect(() => {
-		console.log("Component Mount!");
+		setIsLoading(true);
+		apiClient
+			.get("/users")
+			.then((res) => {
+				setSellers(res.data);
+				setIsLoading(false);
+			})
+			.catch((err) => {
+				setIsLoading(false);
+				setErrors(err.Message);
+			});
+	}, []);
 
-		return () => {
-			console.log("Component Unmount!");
+	const addSeller = () => {
+		const newSeller = {
+			name,
+			id: sellers.length + 1,
 		};
-	}, [name]);
+		setSellers([newSeller, ...sellers]);
+		apiClient
+			.post("/users", newSeller)
+			.then((res) => setSellers([res.data, ...sellers]))
+			.catch((err) => {
+				setErrors(err.message);
+				setSellers(sellers);
+			});
+	};
 
-	// clean function is use to clean up side effects
+	const deleteSeller = (id) => {
+		setSellers(sellers.filter((s) => s.id !== id));
+		apiClient.delete(`/users/${id}`).catch((err) => {
+			setErrors(err.message);
+			setSellers(sellers);
+		});
+	};
+
+	const updateSeller = (seller) => {
+		const updatedSeller = {
+			...seller,
+			name: seller.name + " Updated",
+		};
+		setSellers(sellers.map((s) => (s.id === seller.id ? updatedSeller : s)));
+		apiClient.patch(`/users/${seller.id}`, updateSeller).catch((err) => {
+			setErrors(err.message);
+			setSellers(sellers);
+		});
+	};
 
 	return (
 		<>
 			<h3>Admin Sellers Page</h3>
 			<input type="text" onChange={(e) => setName(e.target.value)} />
+			<button onClick={addSeller}>Add Seller</button>
+			{isLoading && <Loader />}
+			{errors && <em>{errors}</em>}
+			<table>
+				<tbody>
+					{sellers.map((seller) => (
+						<tr key={seller.id}>
+							<td>{seller.name}</td>
+							<td>
+								<button onClick={() => updateSeller(seller)}>Update</button>
+							</td>
+							<td>
+								<button onClick={() => deleteSeller(seller.id)}>Delete</button>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</>
 	);
 };
